1164b6afc81d5a6b9510fa6145130452d0417b45
dims101 <dimas.stmp@gmail.com>
2023-11-19 16:59:11 +0700

    fix register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,14 +50,6 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
                         @else
                         <li class="nav-item"><a href="/home" class="nav-link">Input Invoice</a></li>
                         <li class="nav-item"><a href="/layanan" class="nav-link">Input Harga</a></li>
